089224a18d524b1cbe2c47720b7a4f4d8190ce9c
JoaoSBarbosa <contato.jsbarbosa@gmail.com>
2022-07-13 12:10:45 -0300

    Security config

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -5,7 +5,7 @@ function Header() {
     <header>
       <div className="dsmeta-logo-container">
         <img src={logo} alt="DSMeta" />
-        <h1>DSMeta</h1>
+        <h1>DevMeta</h1>
         <p>
           Desenvolvido por:
           <a
--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import NotificationButton from "../NotificationButton";
 import "./styles.css";
 
 function SalesCard() {
+  const min = new Date(new Date().setDate(new Date().getDate() - 365));
+  const max = new Date();
+
+  const [minDate, setMinDate] = useState(min);
+  const [maxDate, setMaxDate] = useState(max);
+
   return (
     <div className="dsmeta-card">
       <h2 className="dsmeta-sales-title">Vendas</h2>
@@ -11,16 +18,16 @@ function SalesCard() {
       <div>
         <div className="dsmeta-form-control-container">
           <DatePicker
-            selected={new Date()}
-            onChange={(Date: Date) => {}}
+            selected={minDate}
+            onChange={(date: Date) => setMinDate(date)}
             className="dsmeta-form-control"
             dateFormat={"dd/MM/yyyy"}
           />
         </div>
         <div className="dsmeta-form-control-container">
           <DatePicker
-            selected={new Date()}
-            onChange={(Date: Date) => {}}
+            selected={maxDate}
+            onChange={(date: Date) => setMaxDate(date)}
             className="dsmeta-form-control"
             dateFormat={"dd/MM/yyyy"}
           />
@@ -43,7 +50,7 @@ function SalesCard() {
             <tr>
               <td className="show992">#341</td>
               <td className="show576">08/07/2022</td>
-              <td>Anakin</td>
+              <td>João Barbosa</td>
               <td className="show992">15</td>
               <td className="show992">11</td>
               <td>R$ 55300,00</td>
@@ -56,7 +63,7 @@ function SalesCard() {
             <tr>
               <td className="show992">#341</td>
               <td className="show576">09/07/2022</td>
-              <td>Anakin</td>
+              <td>João Barbosa</td>
               <td className="show992">15</td>
               <td className="show992">11</td>
               <td>R$ 55300,00</td>
@@ -69,7 +76,7 @@ function SalesCard() {
             <tr>
               <td className="show992">#341</td>
               <td className="show576">10/07/2022</td>
-              <td>Anakin</td>
+              <td>João Barbosa</td>
               <td className="show992">15</td>
               <td className="show992">11</td>
               <td>R$ 55300,00</td>
