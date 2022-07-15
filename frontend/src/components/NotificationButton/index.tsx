3f8e25e5cbd8b19f5a72afcae8d5a7b23bde032b
JoaoSBarbosa <contato.jsbarbosa@gmail.com>
2022-07-15 12:14:16 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,22 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg' /* dois pontos significa voltar uma pasta*/
+import { BASE_URL } from '../../utils/request';
 import './styles.css' /* um ponto significa mesma pasta */
 
-function NotificationButton() {
+type Props = {
+  saleId: number;
+}
+
+function handClick(id: number) {
+  axios(`${BASE_URL}/sales/${id}/notification`)
+  .then(response => {
+    console.log("SUCESSO");
+  })
+}
+
+function NotificationButton({saleId} : Props) {
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handClick(saleId)}>
       <img src={icon} alt="Notificar" /> 
     </div>
   );
