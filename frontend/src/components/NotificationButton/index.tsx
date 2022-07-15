d5e4f1971587cd02eb5b2f287e71b0e9cbf68a6a
JoaoSBarbosa <contato.jsbarbosa@gmail.com>
2022-07-15 12:22:12 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg' /* dois pontos significa voltar uma pasta*/
 import { BASE_URL } from '../../utils/request';
 import './styles.css' /* um ponto significa mesma pasta */
@@ -10,7 +11,7 @@ type Props = {
 function handClick(id: number) {
   axios(`${BASE_URL}/sales/${id}/notification`)
   .then(response => {
-    console.log("SUCESSO");
+    toast.info(" SMS enviado com sucesso");
   })
 }
 
